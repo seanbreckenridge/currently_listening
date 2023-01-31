adf245673e619255598e9f545b87a637de44c239
Sean Breckenridge <seanbrecke@gmail.com>
2023-01-30 17:25:14 -0800

    discord presence: resolves #3
    only send message to request currently playing first time
    launching the websocket, then just block in the recieve loop
    and recieve broadcasts. if we recieve too many requests
    for the discord rpc server to keep up, rate limit till
    we hit 15 seconds since the last request

--- currently_listening_py/currently_listening_py/discord_presence.py
+++ currently_listening_py/currently_listening_py/discord_presence.py
@@ -1,4 +1,5 @@
 import json
+import time
 from typing import AsyncGenerator
 from asyncio import sleep
 
@@ -39,43 +40,63 @@ class Payload(BaseModel):
 async def get_currently_playing(
     server_url: str,
 ) -> AsyncGenerator[Payload, None]:
+    first: bool = True
     async for websocket in websockets.connect(server_url):
         try:
-            await websocket.send("currently-listening")
+            if first:
+                logger.debug("first loop; sending currently-listening message")
+                await websocket.send("currently-listening")
+                first = False
+            logger.debug("waiting for response")
             response = await websocket.recv()
             response = json.loads(response)
             response = Payload(**response)
             logger.debug(response)
             yield response
-            await sleep(30)
         except websockets.ConnectionClosed:
+            logger.debug("Connection closed, reconnecting")
+            first = True
             await sleep(60)
             continue
 
 
-async def set_discord_presence_loop(server_url: str, client_id: str):
+async def set_discord_presence_loop(
+    server_url: str, client_id: str, discord_rpc_wait_time: int = 20
+):
     RPC = AioPresence(client_id)
     await RPC.connect()
+    logger.debug(await RPC.clear())
     current_state = None
+    # offset first wait time so we dont wait first time updating presence
+    last_request_at = time.time() - discord_rpc_wait_time * 2
+
+    async def rate_limit() -> None:
+        nonlocal last_request_at
+        # should wait 15 seconds between updates
+        sleep_for = discord_rpc_wait_time - (time.time() - last_request_at)
+        if sleep_for > 0:
+            logger.debug(f"Sleeping for {sleep_for} seconds")
+            await sleep(sleep_for)
+        last_request_at = time.time()
+
     async for state in get_currently_playing(server_url):
         if state.data.playing and state.data.song is not None:
             if current_state == state.data:
                 logger.debug("Song is playing, but no change in state")
                 continue
+
+            await rate_limit()
+
             logger.debug("Song is playing, updating presence")
-            kwargs = {}
-            # TODO: add a lock here to prevent duplicate updates?
-            await RPC.update(
-                state=state.data.song.describe(),
-                **kwargs,
-            )
-            # update local state after successful update
             current_state = state.data
+            # TODO: add a lock here to prevent duplicate updates?
+            logger.debug(
+                await RPC.update(
+                    state=state.data.song.describe(),
+                )
+            )
         else:
-            if current_state is not None:
-                logger.debug("Song is not playing, clearing presence")
-                await RPC.clear()
-                # update local state after successful update
-                current_state = None
-            else:
-                logger.debug("Song is not playing, but no change in state")
+            logger.debug("Song is not playing, clearing presence")
+            await rate_limit()
+            current_state = None
+            logger.debug(await RPC.clear())
